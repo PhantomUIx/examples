44b5a8b254a423aeea991850285748aca25ca351
Tristan Ross <tristan.ross@midstall.com>
2023-11-01 19:04:46 -0700

    feat: use new auto-importer

--- src/example.zig
+++ src/example.zig
@@ -1,12 +1,6 @@
 const std = @import("std");
 const phantom = @import("phantom");
 
-pub const phantomOptions = struct {
-    pub const modules = struct {
-        pub const i18n = @import("phantom.i18n");
-    };
-};
-
 pub fn main() void {
     std.debug.print("{}\n", .{phantom.i18n});
 }
