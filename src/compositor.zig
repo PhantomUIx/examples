c5907b2811abeeaafdeacb6d47b20bb4ffbfbaa6
Tristan Ross <tristan.ross@midstall.com>
2023-11-26 15:12:57 -0800

    fix: make buildable for uefi

--- src/compositor.zig
+++ src/compositor.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const options = @import("options");
 const phantom = @import("phantom");
 const vizops = @import("vizops");
@@ -9,14 +10,14 @@ const displayBackend = phantom.display.Backend(displayBackendType);
 const sceneBackendType: phantom.scene.BackendType = @enumFromInt(@intFromEnum(options.scene_backend));
 const sceneBackend = phantom.scene.Backend(sceneBackendType);
 
-pub fn main() !void {
-    const alloc = std.heap.page_allocator;
+pub fn main() void {
+    const alloc = if (builtin.link_libc) std.heap.c_allocator else if (builtin.os.tag == .uefi) std.os.uefi.pool_allocator else std.heap.page_allocator;
 
     var display = displayBackend.Display.init(alloc, .compositor);
     defer display.deinit();
 
     if (displayBackendType == .headless) {
-        _ = try display.addOutput(.{
+        _ = display.addOutput(.{
             .enable = true,
             .size = .{
                 .phys = vizops.vector.Float32Vector2.init([_]f32{ 306, 229.5 }),
@@ -26,26 +27,26 @@ pub fn main() !void {
             .name = "display-0",
             .manufacturer = "PhantomUI",
             .format = try vizops.color.fourcc.Value.decode(vizops.color.fourcc.formats.argb16161616),
-        });
+        }) catch |e| @panic(@errorName(e));
     }
 
-    const outputs = try @constCast(&display.display()).outputs();
+    const outputs = @constCast(&display.display()).outputs() catch |e| @panic(@errorName(e));
     defer outputs.deinit();
 
     if (outputs.items.len == 0) {
-        return error.NoOutputs;
+        @panic("No outputs");
     }
 
     const output = outputs.items[0];
-    const surface = try output.createSurface(.output, .{
-        .size = (try output.info()).size.res,
-    });
+    const surface = output.createSurface(.output, .{
+        .size = (output.info() catch |e| @panic(@errorName(e))).size.res,
+    }) catch |e| @panic(@errorName(e));
     defer {
         surface.destroy() catch {};
         surface.deinit();
     }
 
-    const scene = try surface.createScene(@enumFromInt(@intFromEnum(sceneBackendType)));
+    const scene = surface.createScene(@enumFromInt(@intFromEnum(sceneBackendType))) catch |e| @panic(@errorName(e));
     _ = scene;
     // TODO: render something to the scene
 }
