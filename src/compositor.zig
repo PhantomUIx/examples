87db861cfe53d20ab62d325b971f57eecd2c9540
Tristan Ross <tristan.ross@midstall.com>
2023-11-27 15:58:00 -0800

    feat: parity with display-uefi example

--- src/compositor.zig
+++ src/compositor.zig
@@ -11,6 +11,251 @@ const sceneBackendType: phantom.scene.BackendType = @enumFromInt(@intFromEnum(op
 const sceneBackend = phantom.scene.Backend(sceneBackendType);
 
 pub fn main() void {
+    const colors: []const [17]vizops.color.Any = &.{
+        .{
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0xf7, 0x76, 0x8e, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0xff, 0x9e, 0x64, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0xe0, 0xaf, 0x68, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x9e, 0xce, 0x6a, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x73, 0xda, 0xca, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0xb4, 0xf9, 0xf8, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x2a, 0xc3, 0xde, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x7d, 0xcf, 0xff, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x7a, 0xa2, 0xf7, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0xbb, 0x9a, 0xf7, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0xc0, 0xca, 0xf5, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0xa9, 0xb1, 0xd6, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x9a, 0xa5, 0xce, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0xcf, 0xc9, 0xc2, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x56, 0x5f, 0x89, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x41, 0x48, 0x68, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x1a, 0x1b, 0x26, 0xff },
+                    },
+                },
+            },
+        },
+        .{
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0xf7, 0x76, 0x8e, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0xff, 0x9e, 0x64, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0xe0, 0xaf, 0x68, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x9e, 0xce, 0x6a, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x73, 0xda, 0xca, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0xb4, 0xf9, 0xf8, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x2a, 0xc3, 0xde, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x7d, 0xcf, 0xff, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x7a, 0xa2, 0xf7, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0xbb, 0x9a, 0xf7, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0xc0, 0xca, 0xf5, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0xa9, 0xb1, 0xd6, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x9a, 0xa5, 0xce, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0xcf, 0xc9, 0xc2, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x56, 0x5f, 0x89, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x41, 0x48, 0x68, 0xff },
+                    },
+                },
+            },
+            .{
+                .uint8 = .{
+                    .sRGB = .{
+                        .value = .{ 0x24, 0x28, 0x3b, 0xff },
+                    },
+                },
+            },
+        },
+    };
+
     const alloc = if (builtin.link_libc) std.heap.c_allocator else if (builtin.os.tag == .uefi) std.os.uefi.pool_allocator else std.heap.page_allocator;
 
     var display = displayBackend.Display.init(alloc, .compositor);
@@ -26,7 +271,7 @@ pub fn main() void {
             .scale = vizops.vector.Float32Vector2.init(1.0),
             .name = "display-0",
             .manufacturer = "PhantomUI",
-            .format = try vizops.color.fourcc.Value.decode(vizops.color.fourcc.formats.argb16161616),
+            .colorFormat = vizops.color.fourcc.Value.decode(vizops.color.fourcc.formats.argb16161616) catch |e| @panic(@errorName(e)),
         }) catch |e| @panic(@errorName(e));
     }
 
@@ -47,6 +292,31 @@ pub fn main() void {
     }
 
     const scene = surface.createScene(@enumFromInt(@intFromEnum(sceneBackendType))) catch |e| @panic(@errorName(e));
-    _ = scene;
-    // TODO: render something to the scene
+
+    var children: [17]*phantom.scene.Node = undefined;
+
+    for (&children, colors[0]) |*child, color| {
+        child.* = scene.createNode(.NodeRect, .{
+            .color = color,
+            .size = vizops.vector.Float32Vector2.init([_]f32{ 100.0 / 17.0, 100.0 }),
+        }) catch |e| @panic(@errorName(e));
+    }
+
+    const flex = scene.createNode(.NodeFlex, .{
+        .direction = phantom.painting.Axis.horizontal,
+        .children = &children,
+    }) catch |e| @panic(@errorName(e));
+    defer flex.deinit();
+
+    var i: usize = 0;
+    while (i < 5) : (i += 1) {
+        _ = scene.frame(flex) catch |e| @panic(@errorName(e));
+
+        const currPalette = scene.seq % colors.len;
+        for (children, colors[currPalette]) |child, color| {
+            child.setProperties(.{ .color = color }) catch |e| @panic(@errorName(e));
+        }
+
+        flex.setProperties(.{ .children = &children }) catch |e| @panic(@errorName(e));
+    }
 }
